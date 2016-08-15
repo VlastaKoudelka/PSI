dcf3cdb891b32721cb882ae56822c425d95cc5ee
VlastaKoudelka <vlasta.koudelka@gmail.com>
2016-08-15 13:35:57 +0200

    Selected pairs/silhouette crit/dynamic networks

--- SNEpy.py
+++ SNEpy.py
@@ -7,31 +7,38 @@ Created on Wed Aug 10 13:59:44 2016
 from scipy.cluster.vq import kmeans2
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import matplotlib.cm as cm
 import scipy.io as sio
+import sklearn.metrics as sk
 import tsne
 plt.close('all')
 
 perplexity = 5
 init_dims = 18
 no_dims = 2
+no_clstr = 3
+no_pairs = 36
+
 
 #Import matlab files
-drug_mat = sio.loadmat('D:\Filip_PSI_mysi\Coherence_StatisticFinalTables\MDL.mat')    #open the .mat
-coord_mat = sio.loadmat('pair_coord_source.mat')
+drug_mat = sio.loadmat('D:\Filip_PSI_mysi\Coherence_StatisticFinalTables\SB.mat')    #open the .mat
+coord_mat = sio.loadmat('Locations.mat')
+brain = mpimg.imread('brain2.png')
 
 
-x = np.array(coord_mat['x_paircoord'])
-y = np.array(coord_mat['y_paircoord'])
+x = np.array(coord_mat['x_pair_sel'])       #x_pair_all for all pairs
+y = np.array(coord_mat['y_pair_sel'])
+index = np.array(coord_mat['index'][0])     #indices of used electrode pairs
 
-
-drug_str = np.array(drug_mat['MDL'][0])            #opent the structure
-
+drug_str = np.array(drug_mat['SB'][0])            #opent the structure
 data = np.array(drug_str['data'][0])
-
 names = drug_str['namesOfElecs']
+
+data = data[:,:,index]                      #select the used electrodes
 average = np.mean(data,0).T
 
-coh = np.zeros([4,6,66])    #band,time,pair
+coh = np.zeros([4,6,no_pairs])    #band,time,pair
 
 #select the bands
 for i,val in enumerate(np.arange(0,56,14)):
@@ -39,27 +46,51 @@ for i,val in enumerate(np.arange(0,56,14)):
 
 #Diferentiate the data in time
 diff_coh = np.diff(coh,1,0)
-diff_coh_res = np.reshape(diff_coh,[18,66],'F').T        
-coh_res = np.reshape(coh,[24,66],'F').T
+diff_coh_res = np.reshape(diff_coh,[18,no_pairs],'F').T        
+coh_res = np.reshape(coh,[24,no_pairs],'F').T
 
 
 #t-SNE on dataset
 mapped = tsne.tsne(diff_coh_res, no_dims, init_dims, perplexity)
 
-labels = kmeans2(mapped,3,10)
+#Clusterring
+labels = kmeans2(mapped,no_clstr,10)[1]
 
-#Show the coherence clusters
+#Silhoulette criterion
+clust_crit = np.zeros([len(labels),1])
+for i in np.arange(2,len(labels)):
+    labels2 = kmeans2(mapped,i,iter = 10)[1]
+    clust_crit[i] = sk.silhouette_score(mapped,labels2)
+
+
+#---Show the coherence clusters
+colors = cm.jet(np.linspace(0, 1, no_clstr))
+
 plt.figure(0)
-plt.scatter(mapped[:,0],mapped[:,1],c = labels[1], s = 150,alpha = 0.5)
+plt.scatter(mapped[:,0],mapped[:,1],c = colors[labels], s = 100,alpha = 0.5)
 
+#---Show electrode numbers
 for i,coord in enumerate(mapped):
-    plt.text(coord[0],coord[1],str(names[0][i][0][0]))
-plt.show
+    plt.text(coord[0],coord[1],str(names[0][index[i]][0][0]))
+    plt.show
 
+#---Show brain
+plt.figure(1)
+plt.imshow(brain)
 
-plt.figure(4)
+for i in np.arange(len(x)):
+    plt.plot(x[i],y[i], c = colors[labels[i]],linewidth = 2)  
+    plt.show
+        
+#---Show difference matrix
+plt.figure(2)
 plt.matshow(diff_coh_res)
 
-
-plt.figure(5)
+#---Show coherence matrix
+plt.figure(3)
 plt.matshow(coh_res)
+
+#---Show Silhoulette
+plt.figure(5)
+plt.plot(clust_crit)
+plt.show
--- docoordinates.m
+++ docoordinates.m
@@ -1,21 +1,60 @@
+load pair_coord_source x y pairs labels
+load el_names electrodes
 
+%% all electrodes
 for i = 1:size(pairs,1)
     for j = 1:length(labels)
         for k = 1:length(labels)
-            str = [labels{j},'-',labels{k}]
+            str = [labels{j},'-',labels{k}];
             if strcmp(str,pairs{i})
-                x_paircoord(i,1) = x(j);
-                x_paircoord(i,2) = x(k);
-                y_paircoord(i,1) = y(j);
-                y_paircoord(i,2) = y(k);
+                x_pair_all(i,1) = x(j);
+                x_pair_all(i,2) = x(k);
+                y_pair_all(i,1) = y(j);
+                y_pair_all(i,2) = y(k);
+            end
+        end
+    end
+end
+% 
+% brain = imread('brain2.png');
+% imagesc(brain)
+% 
+% for i = 1:length(pairs)
+%     line(x_paircoord(i,:),y_paircoord(i,:))
+% end
+
+%% selected electrodes
+
+for i = 1:size(electrodes,1)
+    for j = 1:length(labels)
+        for k = 1:length(labels)
+            str = [labels{j},' ',labels{k}];
+            if strcmp(str,electrodes(i,:))
+                x_pair_sel(i,1) = x(j);
+                x_pair_sel(i,2) = x(k);
+                y_pair_sel(i,1) = y(j);
+                y_pair_sel(i,2) = y(k);
             end
         end
     end
 end
 
+
+for i = 1:size(pairs,1)
+    for j = 1:size(electrodes,1)
+        str = [electrodes(j,1:2),'-',electrodes(j,4:5)];
+        if strcmp(str,pairs{i})
+            index(j) = i - 1;
+        end
+    end
+end       
+
 brain = imread('brain2.png');
 imagesc(brain)
 
-for i = 1:length(pairs)
-    line(x_paircoord(i,:),y_paircoord(i,:))
+for i = 1:length(electrodes)
+    line(x_pair_sel(i,:),y_pair_sel(i,:))
 end
+
+save Locations x_pair_all x_pair_sel y_pair_all y_pair_sel index
+       
