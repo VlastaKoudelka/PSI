e6074d1f33e05952b957409836c3f08d1a4abe41
Student1 <Student1@pcp.lf3.cuni.cz>
2017-06-05 18:18:44 +0200

    New smart Loader picks interpolated files

--- load_BV_export.m
+++ load_BV_export.m
@@ -10,14 +10,28 @@ for i = 1:nSubj
 end
 
 %% load appropriate data from BVA export
-path = 'D:\Psilocybin all data\export_záloha\';
+path = 'D:\Vlasta\PSI_OZ_EDF\';
 
 nEpoch = length(fileNames{1});
 
 for i = 1:nSubj
     for j = 1:length(fileNames{i}) 
 %         dir([path fileNames{i}(j).name(1:20) '*.dat'])
-        dir([path fileNames{i}(j).name(1:14) '*' fileNames{i}(j).name(15:20)  '*.dat'])
+       filea = dir([path fileNames{i}(j).name(1:14) '*' fileNames{i}(j).name(15:20)  '*embed.edf']);
+       fileb = dir([path fileNames{i}(j).name(1:14) '*' fileNames{i}(j).name(15:20)  '*long.edf']);
+       
+       if length(fileb)>0   %If a long version exist
+           filea = fileb;   %use the long version
+       end
+       
+       if length(filea)>0   %This will be probably always true
+           SubFile(j) = filea;
+           clear filea
+       else
+           clear filea
+       end                    
     end
+    GroupFile{i}=SubFile;
+    clear SubFile;
 end
         
